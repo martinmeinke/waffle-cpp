e0bfebb41d40a44d5784d2b011cdd5b8a6b2e834
Martin Meinke <meinke.martin@googlemail.com>
2019-10-10 19:37:57 +0200

    add GridSearchVariadic to experiment with storing parameters in a tuple

--- include/waffle/waffle.h
+++ include/waffle/waffle.h
@@ -1,10 +1,64 @@
 #pragma once
 
 #include <string>
-
+#include <iostream>
+#include <tuple>
 
 namespace waffle
 {
     std::string waffle();
+
+    // random access iterator -> relax to fwd iterator at some point
+    template <typename IteratorT>
+    struct Parameter{
+        std::string name_;
+        IteratorT start_; // use lazy range?
+        IteratorT end_; // use lazy range?
+        int index_ = 0;
+
+        Parameter(std::string name, IteratorT start, IteratorT end) : name_(name), start_(start), end_(end)
+        {
+
+        }
+
+        // operator ++?
+
+        IteratorT get(){
+            return start_ + index_;
+        }       
+    };
+
+    /*template <typename ParameterListT, typename ObjectiveFctr>
+    struct GridSearch {
+        public:
+            GridSearch(ParameterListT plt, ObjectiveFctr of) : parameters_(plt), objective_(of) {}
+            void run(){
+                std::cout << objective_(1, "hallo", 2.0) << std::endl;
+            }
+
+        private:
+            ParameterListT parameters_;
+            ObjectiveFctr objective_;
+    };*/
+
+    template <typename ObjectiveFctr, typename... ParameterTs>
+    struct GridSearchVariadic {
+        using ParameterTuple = std::tuple<ParameterTs...>;
+        public:
+            GridSearchVariadic(ObjectiveFctr of, ParameterTs... params) : 
+                objective_(of), 
+                parameters_(params...) {
+                
+            }
+            void run(){
+                std::cout << objective_(1, "hallo", 2.0) << std::endl;
+                std::cout << *std::get<0>(parameters_).get() << std::endl;
+                std::cout << std::tuple_size<ParameterTuple>::value << std::endl;
+            }
+
+        private:
+            ObjectiveFctr objective_;
+            ParameterTuple parameters_;
+    };
 }
 
--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,15 +1,53 @@
+#include <variant>
+
 #include <gtest/gtest.h>
 #include <waffle/waffle.h>
 
 TEST(WaffleTest, dummy)
 {
-    ASSERT_TRUE(true);
+    ASSERT_EQ("waffle", waffle::waffle());
 }
+
+struct Objective{
+  float operator()(int steps, std::string algo, float decay)
+  {
+    return static_cast<float>(steps) / decay;
+  }
+};
 
 TEST(WaffleTest, waffle)
 {
-    ASSERT_EQ("waffle", waffle::waffle());
+    using namespace waffle;
+    std::vector<int> p1_vals{1,2,3};
+    Parameter p1{"steps", p1_vals.begin(), p1_vals.end()};
+
+    std::vector<std::string> p2_vals{"algo1", "balgo"};
+    Parameter p2{"algorithm", p2_vals.begin(), p2_vals.end()};
+
+    std::vector<float> p3_vals{0.01, 0.001, 0.0001};
+    Parameter p3{"decay", p3_vals.begin(), p3_vals.end()};
+
+    // the variant to visit
+    /*using ParameterVariant = std::variant<
+      Parameter<std::vector<int>>, 
+      Parameter<std::vector<std::string>>, 
+      Parameter<std::vector<float>>>;
+    using ParameterList = std::vector<ParameterVariant>;
+
+    ParameterList parameter_list = {p1, p2, p3};
+    Objective o;
+    GridSearch<ParameterList, Objective> gs{parameter_list, o};*/
+    
+    Objective o;
+    GridSearchVariadic gs{o, p1, p2, p3};
+    gs.run();
+
 }
+
+/*TEST(WaffleTest, waffle_wunschapi)
+{
+    GridSearch(scoring_functor, {{{"p1", range(1.0,20.0,0.5)}}});
+}*/
 
 int main(int argc, char **argv)
 {
